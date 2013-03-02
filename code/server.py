f86cd058e00ba6f038432c75befcb793a1f8582c
Pimin Konstantin Kefaloukos <skipperkongen@gmail.com>
2013-03-02 15:03:20 +0100

    simpler, better, query engine needs work

--- code/server.py
+++ code/server.py
@@ -24,18 +24,16 @@ class WebCoProcessorHandler(tornado.websocket.WebSocketHandler):
 	real-time web sites about 1000% easier than it is today.
 	"""
 	def initialize(self, webcoprocessor):
-		self.webcoprocessor
-		self.engine = None
-
-	next_webcore_id = 1
+		self.webcoprocessor = webcoprocessor
+		self.core_capabilities = None
 	
 	def write_message_json(self, obj):
-		self.write_message json_dumps(obj.__dict__)
+		self.write_message( json_dumps(obj.__dict__) )
 	
 	def open(self):
 		# pdb.set_trace() # let's see what info we have
 		print "webcore joining"
-		self.webcoprocessor.add_core(self)
+		self.webcoprocessor.add_core( self )
 		
 	def on_message(self, message):
 		"""Received a message from a web core"""
@@ -45,7 +43,7 @@ class WebCoProcessorHandler(tornado.websocket.WebSocketHandler):
 
 		if msg_from_core.message_type == 'init':
 			# web core wanna to tell us something about its flops
-			self.engine = msg_from_core.data['engine']
+			self.core_capabilities = msg_from_core.data['core_capabilities']
 
 		if msg_from_core.message_type == 'result':
 			# web core has computed a JSON result
@@ -58,6 +56,20 @@ class WebCoProcessorHandler(tornado.websocket.WebSocketHandler):
 		print "webcore leaving"
 		self.webcoprocessor.remove_core(self)
 
+class QueryEngineHandler(tornado.websocket.WebSocketHandler):
+	
+	def initialize(self, queryengine):
+		self.queryengine = queryengine
+		
+	def open(self):
+		#self.write_message("not implemented")
+		self.queryengine.add_connection( self )
+	
+	def on_message(self, message):
+		#self.write_message("not implemented")	
+
+	def on_close(self):
+
 class WCPMessage(Object):
 	"""Based on anonymous object"""
 	
@@ -69,20 +81,26 @@ class WebCoProcessor(object):
 	"""docstring for WebCoProcessor"""
 	def __init__(self, input_buffer, output_buffer):
 		super(WebCoProcessor, self).__init__()
-		self.cores = dict()
+		self.cores = set()
 		self.input_buffer = input_buffer
 		self.output_buffer = output_buffer
 	
 	def add_core(self, core):
 		self.cores.add( core )	
 
-	def remove_core(self, websocket):
-		self.cores.remove( Core(websocket) )
+	def remove_core(self, core):
+		self.cores.remove( core )
 	
 	def start(self):
 		"""in thread: while 1: get job from input_buffer, partition and send to cores"""
 		pass
 		
+class QueryEngine(object):
+	"""docstring for WCPQueryEngine"""
+	def __init__(self, webcoprocessor):
+		super(QueryEngine, self).__init__()
+		self.webcoprocessor = webcoprocessor
+
 if __name__ == "__main__":
 	
 	settings = {
@@ -91,13 +109,13 @@ if __name__ == "__main__":
 	}
 	
 	# TODO, add persistence via leveldb or tightdb
-	webcoprocessor = WebCoProcessor( input_buffer=Queue(), outout_buffer=Queue() ) # The web co-processor (WCP)
-	queryengine = WCPQueryEngine( webcoprocessor ) # query engine executes queries on WCP
+	webcoprocessor = WebCoProcessor( input_buffer=Queue(), output_buffer=Queue() ) # The web co-processor (WCP)
+	queryengine = QueryEngine( webcoprocessor ) # query engine executes queries on WCP
 	
 	application = tornado.web.Application([
 		(r"/demo", DemoHandler),
-		(r'/webcoprocessor/attach', WebCoProcessorHandler, dict(webcoprocessor=webcoprocessor))
-		(r'/queryengine', QueryEngineHandler, queryengine=queryengine)
+		(r'/webcoprocessor/attach', WebCoProcessorHandler, dict(webcoprocessor=webcoprocessor)),
+		(r'/queryengine/attach', QueryEngineHandler, dict(queryengine=queryengine))
 	],**settings)
 
 	application.listen(8888)
